792fd3252ba109647d0debbddd4076a5166121a9
Adel Medhat <adelmedhat809@gmail.com>
2024-02-15 09:30:44 +0200

    * Finish Hadeth Feature

---
+++ lib/moduls/hadeth/page/hadeth_details_view.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:islami/moduls/hadeth/page/hadeth_view.dart';
+
+class HadethDetailsView extends StatelessWidget {
+  static const routeName = "hadethDetails";
+
+  const HadethDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)?.settings.arguments as HadethData;
+    var mediaQuery = MediaQuery.of(context).size;
+    var theme = Theme.of(context);
+
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            "assets/images/main_background.png",
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "إسلامي",
+          ),
+        ),
+        body: Container(
+          width: mediaQuery.width,
+          height: mediaQuery.height,
+          margin: const EdgeInsets.only(
+            right: 20,
+            left: 20,
+            top: 15,
+            bottom: 80,
+          ),
+          padding: const EdgeInsets.only(
+            right: 20,
+            left: 20,
+            top: 15,
+            bottom: 15,
+          ),
+          decoration: BoxDecoration(
+            color: const Color(0xFFF8F8F8).withOpacity(0.8),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Column(
+            children: [
+              Text(
+                args.title,
+                style: const TextStyle(
+                  fontFamily: "El Messiri",
+                  fontWeight: FontWeight.w600,
+                  fontSize: 25,
+                ),
+              ),
+              Divider(
+                color: theme.primaryColor,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Text(
+                    args.content,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontFamily: "El Messiri",
+                      fontWeight: FontWeight.w500,
+                      fontSize: 23,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/moduls/hadeth/page/hadeth_view.dart
+++ lib/moduls/hadeth/page/hadeth_view.dart
@@ -1,20 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/moduls/hadeth/page/hadeth_details_view.dart';
 
-class HadethView extends StatelessWidget {
+class HadethView extends StatefulWidget {
   const HadethView({super.key});
 
   @override
+  State<HadethView> createState() => _HadethViewState();
+}
+
+class _HadethViewState extends State<HadethView> {
+  List<HadethData> allHadethContent = [];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Hadeth View",
-        style: TextStyle(
-          color: Colors.black,
-          fontFamily: "El Messiri",
-          fontWeight: FontWeight.bold,
-          fontSize: 30,
+    var mediaQuery = MediaQuery.of(context).size;
+    var theme = Theme.of(context);
+
+    if (allHadethContent.isEmpty) loadData();
+    return Column(
+      children: [
+        Image.asset(
+          "assets/images/hadeth_header.png",
+          height: mediaQuery.height * 0.2,
         ),
-      ),
+        Divider(
+          color: theme.primaryColor,
+          thickness: 1.2,
+        ),
+        const Text(
+          "الأحاديث",
+          style: TextStyle(
+            fontFamily: "El Messiri",
+            fontSize: 25,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Divider(
+          color: theme.primaryColor,
+          thickness: 1.2,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) => InkWell(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  HadethDetailsView.routeName,
+                  arguments: allHadethContent[index],
+                );
+              },
+              child: Text(
+                allHadethContent[index].title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: "El Messiri",
+                  fontSize: 23,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            itemCount: allHadethContent.length,
+          ),
+        )
+      ],
     );
   }
+
+  Future<void> loadData() async {
+    String content = await rootBundle.loadString("assets/files/ahadeth.txt");
+    List<String> allHadethList = content.split("#");
+    for (int i = 0; i < allHadethList.length; i++) {
+      String singleHadeth = allHadethList[i].trim();
+
+      int titelIndex = singleHadeth.indexOf("\n");
+
+      String title = singleHadeth.substring(0, titelIndex);
+      String content = singleHadeth.substring(titelIndex + 1);
+
+      HadethData hadethData = HadethData(title, content);
+      allHadethContent.add(hadethData);
+    }
+    setState(() {});
+  }
+}
+
+class HadethData {
+  final String title;
+  final String content;
+
+  HadethData(this.title, this.content);
 }
